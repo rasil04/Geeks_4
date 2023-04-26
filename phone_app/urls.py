07fadc14cde031af289ddc30f5a85d4aabd77ef6
rasil04 <den-dmx@mail.ru>
2023-04-26 22:50:17 +0600

    HW5

--- phone_app/urls.py
+++ phone_app/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path
-from phone_app.views import phone_all_view, phone_detail_view, create_phone_view, delete_phone_list_view,\
-    phone_delete_detail_veiw, delete_phone_view, update_phone_view
+from . import views
+# from phone_app.views import phone_all_view, phone_detail_view, create_phone_view, delete_phone_list_view,\
+#     phone_delete_detail_veiw, delete_phone_view, update_phone_view
 
 urlpatterns = [
-    path('phone_list/', phone_all_view, name='phone_list'),
-    path('phone_list/<int:id>/', phone_detail_view, name='phone_detail'),
-    path('add-phone/', create_phone_view, name='create_phone'),
-    path('phone_delete_list/', delete_phone_list_view, name='delete_phone_list'),
-    path('phone_delete_list/<int:id>/', phone_delete_detail_veiw, name='delete_phone_id'),
-    path('phone_delete_list/<int:id>/delete/', delete_phone_view, name='delete_phone_view'),
-    path('phone_update_list/<int:id>/update/', update_phone_view, name='update_phone_view')
+    path('', views.PhoneListView.as_view(), name='phone_list'),
+    path('phone_list/<int:id>/', views.PhoneDetailView.as_view(), name='phone_detail'),
+    path('add-phone/', views.CreatePhoneView.as_view(), name='create_phone'),
+    path('phone_delete_or_update_list/', views.DeleteOrUpdatePhoneListView.as_view(), \
+         name='delete_or_update_phone_list'),
+    path('phone_delete/<int:id>/delete/', views.DeletePhoneView.as_view(), name='delete_phone_view'),
+    path('phone_update/<int:id>/update/', views.UpdatePhoneView.as_view(), name='update_phone_view'),
+    path('search/', views.SearchView.as_view(), name='search')
 ]
+
+
+
+# urlpatterns = [
+#     # path('', phone_all_view, name='phone_list'),
+#     # path('phone_list/<int:id>/', phone_detail_view, name='phone_detail'),
+#     # path('add-phone/', create_phone_view, name='create_phone'),
+#     # path('phone_delete_list/', delete_phone_list_view, name='delete_phone_list'),
+#     # path('phone_delete_list/<int:id>/', phone_delete_detail_veiw, name='delete_phone_id'),
+#     # path('phone_delete_list/<int:id>/delete/', delete_phone_view, name='delete_phone_view'),
+#     # path('phone_update_list/<int:id>/update/', update_phone_view, name='update_phone_view')
+# ]
+
--- phone_app/views.py
+++ phone_app/views.py
@@ -2,67 +2,133 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from phone_app.models import Phones
 from phone_app.forms import ShowForm
+from django.views import generic
 
 # Не полная информация
-def phone_all_view(request):
-    phone_list = Phones.objects.all()
-    context = {
-        'phone_list': phone_list
-    }
-    return render(request, 'phone_list.html', context)
+class PhoneListView(generic.ListView):
+    template_name = 'phone_list.html'
+    queryset = Phones.objects.all()
+
+    def get_queryset(self):
+        return Phones.objects.all()
+
+# def phone_all_view(request):
+#     phone_list = Phones.objects.all()
+#     context = {
+#         'phone_list': phone_list
+#     }
+#     return render(request, 'phone_list.html', context)
 
 # Детальная информация
-def phone_detail_view(request,id):
-    phone_id = get_object_or_404(Phones, id=id)
-    context = {
-        'phone_id': phone_id
-    }
-    return render(request, 'phone_detail.html', context)
+class PhoneDetailView(generic.DetailView):
+    template_name = 'phone_detail.html'
+
+    def get_object(self, **kwargs):
+        phone_id = self.kwargs.get('id')
+        return get_object_or_404(Phones, id=phone_id)
+
+# def phone_detail_view(request,id):
+#     phone_id = get_object_or_404(Phones, id=id)
+#     context = {
+#         'phone_id': phone_id
+#     }
+#     return render(request, 'phone_detail.html', context)
 
 #Добавить новый телефон
-def create_phone_view(request):
-    method = request.method
-    if method == "POST":
-        form = ShowForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Успешно добавлено!')
-    else:
-        form = ShowForm()
-    return render(request, 'crud/create_phone.html', {'form': form})
+class CreatePhoneView(generic.CreateView):
+    template_name = 'crud/create_phone.html'
+    form_class = ShowForm
+    queryset = Phones.objects.all()
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(CreatePhoneView, self).form_valid(form=form)
+
+# def create_phone_view(request):
+#     method = request.method
+#     if method == "POST":
+#         form = ShowForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Успешно добавлено!')
+#     else:
+#         form = ShowForm()
+#     return render(request, 'crud/create_phone.html', {'form': form})
 
 #Список телефонов для удаления
-def delete_phone_list_view(request):
-    phone_list_delete = Phones.objects.all()
-    return render(request, 'crud/phone_list_delete_or_update.html', {'phone_lst_delete': phone_list_delete})
+class DeleteOrUpdatePhoneListView(generic.ListView):
+    template_name = 'crud/phone_list_delete_or_update.html'
+    queryset = Phones.objects.all()
+
+    def get_queryset(self):
+        return Phones.objects.all()
+
+# def delete_phone_list_view(request):
+#     phone_list_delete = Phones.objects.all()
+#     return render(request, 'crud/phone_list_delete_or_update.html', {'phone_lst_delete': phone_list_delete})
 
 #Список для удаления по id
-def phone_delete_detail_veiw(request, id):
-    phone_delete_id = get_object_or_404(Phones, id=id)
-    return render(request, 'crud/phone_id_delete.html',
-                  {'phone_id_delete': phone_delete_id})
+# def phone_delete_detail_veiw(request, id):
+#     phone_delete_id = get_object_or_404(Phones, id=id)
+#     return render(request, 'crud/phone_delete.html',
+#                   {'phone_id_delete': phone_delete_id})
 
 #Удалить телефон основная логика
-def delete_phone_view(request, id):
-    phone_id = get_object_or_404(Phones, id=id)
-    phone_id.delete()
-    return HttpResponse('Удалено!')
+class DeletePhoneView(generic.DeleteView):
+    template_name = 'crud/phone_delete.html'
+    success_url = '/phone_delete_or_update_list/'
+
+    def get_object(self, **kwargs):
+        phone_id = self.kwargs.get('id')
+        return get_object_or_404(Phones, id=phone_id)
+
+# def delete_phone_view(request, id):
+#     phone_id = get_object_or_404(Phones, id=id)
+#     phone_id.delete()
+#     return HttpResponse('Удалено!')
 
 
 #Изменить фильм
-def update_phone_view(request, id):
-    phone_id = get_object_or_404(Phones, id=id)
-    if request.method == 'POST':
-        form = ShowForm(instance=phone_id, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Телефон успешно изменен!')
-    else:
-        form = ShowForm(instance=phone_id)
+class UpdatePhoneView(generic.UpdateView):
+    template_name = 'crud/phone_update.html'
+    form_class = ShowForm
+    success_url = '/phone_delete_or_update_list/'
 
-    context = {
-        'form': form,
-        'phone_id': phone_id
-    }
+    def get_object(self, **kwargs):
+        phone_id = self.kwargs.get('id')
+        return get_object_or_404(Phones, id=phone_id)
 
-    return render(request, 'crud/phone_update.html', context)
+    def form_valid(self, form):
+        return super(UpdatePhoneView, self).form_valid(form=form)
+
+# def update_phone_view(request, id):
+#     phone_id = get_object_or_404(Phones, id=id)
+#     if request.method == 'POST':
+#         form = ShowForm(instance=phone_id, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Телефон успешно изменен!')
+#     else:
+#         form = ShowForm(instance=phone_id)
+#
+#     context = {
+#         'form': form,
+#         'phone_id': phone_id
+#     }
+#
+#     return render(request, 'crud/phone_update.html', context)
+
+#Поисковик
+class SearchView(generic.ListView):
+    template_name = 'phone_list.html'
+    context_object_name = 'phone'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Phones.objects.filter(title__icontains=self.request.GET.get("q"))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
